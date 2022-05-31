3006af6f1fffb6d995d45bcc250d22be891fbeb4
asheikh184 <50655280+asheikh184@users.noreply.github.com>
2022-05-31 17:35:14 +0500

    done

--- src/screens/Editor.jsx
+++ src/screens/Editor.jsx
@@ -1,15 +1,18 @@
-import Header from '../components/header/Header';
-import CanvasBody from '../components/CanvasBody';
-import { Stack } from '@chakra-ui/layout';
+// import Header from '../components/header/Header';
+// import CanvasBody from '../components/CanvasBody';
+// import { Stack } from '@chakra-ui/layout';
 import ResponsiveDrawer from '../components/Drawer';
 
 const Editor = () => {
   return (
-    <Stack margin={'0px !important'} width={'100%'} height={'100vh'}>
-      {/* <Header />
-      <CanvasBody /> */}
+    // <Stack margin={'0px !important'} width={'100%'} height={'100vh'}>
+    //   {/* <Header />
+    //   <CanvasBody /> */}
+    //   <ResponsiveDrawer />
+    // </Stack>
+    <>
       <ResponsiveDrawer />
-    </Stack>
+    </>
   );
 };
 
