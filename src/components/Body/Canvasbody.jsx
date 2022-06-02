30b8246cc127a59de43cfe9241f70b48a27fe3f7
sohaib57 <sohaibbutt57@gmail.com>
2022-06-02 16:05:41 +0500

    canva

--- src/components/Body/Canvasbody.jsx
+++ src/components/Body/Canvasbody.jsx
@@ -1,109 +1,102 @@
-import React, { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { fabric } from 'fabric';
-import { AbsoluteCenter, ButtonGroup, Stack } from '@chakra-ui/react';
-import canvasbg from '../../assests/images/canvasbg.png';
-import { animate } from 'framer-motion';
-import { Button } from '@chakra-ui/react';
 
 const Canvasbody = () => {
-  const canvas = new fabric.Canvas('canvas-main', { selection: false });
-
-  // var rect, isDown, origX, origY;
-
-  // canvas.on('mouse:down', function(o){
-  //     isDown = true;
-  //     var pointer = canvas.getPointer(o.e);
-  //     origX = pointer.x;
-  //     origY = pointer.y;
-  //     var pointer = canvas.getPointer(o.e);
-  //     rect = new fabric.Rect({
-  //         left: origX,
-  //         top: origY,
-  //         originX: 'left',
-  //         originY: 'top',
-  //         width: pointer.x-origX,
-  //         height: pointer.y-origY,
-  //         angle: 0,
-  //         fill: 'rgba(255,0,0,0.5)',
-  //         transparentCorners: false
-  //     });
-  //     canvas.add(rect);
-  // });
-
-  // canvas.on('mouse:move', function(o){
-  //     if (!isDown) return;
-  //     var pointer = canvas.getPointer(o.e);
-
-  //     if(origX>pointer.x){
-  //         rect.set({ left: Math.abs(pointer.x) });
-  //     }
-  //     if(origY>pointer.y){
-  //         rect.set({ top: Math.abs(pointer.y) });
-  //     }
-
-  //     rect.set({ width: Math.abs(origX - pointer.x) });
-  //     rect.set({ height: Math.abs(origY - pointer.y) });
-
-  //     canvas.renderAll();
-  // });
-
-  // canvas.on('mouse:up', function(o){
-  //   isDown = false;
-  // });
-
-  const showrect = () => {
-
-    const rect = new fabric.Rect({
-      height: 280,
-      width: 200,
-      stroke: 'black',
-    })
-    // ,
-    // rect.animate('left', '+=400', {
-    //   onChange: canvas.renderAll.bind(canvas),
-    // });
-    canvas.add(rect);
+  const [oki, setoki] = useState('');
+  const okay = () => {
+    setoki('hehe');
   };
 
-  const circ = new fabric.Circle({
-    radius: 100,
-    fill: 'red',
-  });
+  useEffect(() => {
+    return () => {
+      const canvas = new fabric.Canvas('canvas-main');
 
-  canvas.add(circ);
+      // , { selection: false }
+      // var rect, isDown, origX, origY;
 
-  const textbox = new fabric.Textbox('Click on the Rectangle to move it.', {
-    fontSize: 20,
-    left: 50,
-    top: 100,
-    width: 200,
-    fill: 'black',
-    color: 'white',
-  });
-  canvas.add(textbox);
+      // canvas.on('mouse:down', function(o){
+      //     isDown = true;
+      //     var pointer = canvas.getPointer(o.e);
+      //     origX = pointer.x;
+      //     origY = pointer.y;
+      //     var pointer = canvas.getPointer(o.e);
+      //     rect = new fabric.Rect({
+      //         left: origX,
+      //         top: origY,
+      //         originX: 'left',
+      //         originY: 'top',
+      //         width: pointer.x-origX,
+      //         height: pointer.y-origY,
+      //         angle: 0,
+      //         fill: 'rgba(255,0,0,0.5)',
+      //         transparentCorners: false
+      //     });
+      //     canvas.add(rect);
+      // });
 
-  canvas.add(
-    new fabric.IText('Tap and Type', {
-      fontFamily: 'arial black',
-      left: 100,
-      top: 100,
-    })
-  );
+      // canvas.on('mouse:move', function(o){
+      //     if (!isDown) return;
+      //     var pointer = canvas.getPointer(o.e);
 
-  var path = new fabric.Path('M 0 0 L 200 100 L 170 200 z');
-  path.set({ fill: 'red', stroke: 'green', opacity: 0.5 });
-  canvas.add(path);
+      //     if(origX>pointer.x){
+      //         rect.set({ left: Math.abs(pointer.x) });
+      //     }
+      //     if(origY>pointer.y){
+      //         rect.set({ top: Math.abs(pointer.y) });
+      //     }
 
-  canvas.on({
-    'selection:created': () => {},
-  });
+      //     rect.set({ width: Math.abs(origX - pointer.x) });
+      //     rect.set({ height: Math.abs(origY - pointer.y) });
 
-  // When a selection is cleared
-  canvas.on({
-    'selection:cleared': () => {},
-  });
+      //     canvas.renderAll();
+      // });
 
-  // Rmove the active object on clicking the delete button
+      // canvas.on('mouse:up', function(o){
+      //   isDown = false;
+      // });
+
+      const rect = new fabric.Rect({
+        height: 280,
+        width: 200,
+        stroke: 'black',
+      });
+
+      canvas.add(rect);
+
+      rect.animate('left', '+=400', {
+        onChange: canvas.renderAll.bind(canvas),
+      });
+
+      const circ = new fabric.Circle({
+        radius: 100,
+        fill: 'red',
+      });
+
+      canvas.add(circ);
+
+      const textbox = new fabric.Textbox('Click on the Rectangle to move it.', {
+        fontSize: 20,
+        left: 50,
+        top: 100,
+        width: 200,
+        fill: 'black',
+        color: 'white',
+      });
+      canvas.add(textbox);
+
+      canvas.add(
+        new fabric.IText('Tap and Type', {
+          fontFamily: 'arial black',
+          left: 100,
+          top: 100,
+        })
+      );
+
+      var path = new fabric.Path('M 0 0 L 200 100 L 170 200 z');
+      path.set({ fill: 'red', stroke: 'green', opacity: 0.5 });
+      canvas.add(path);
+    };
+  }, [oki]);
 
   return (
     <>
@@ -114,8 +107,7 @@ const Canvasbody = () => {
         height="600px"
       />
 
-      <input type="button" id="delete" value="Delete selection" />
-      <Button onClick={showrect}>Click me</Button>
+      <button onClick={okay}>Click me</button>
     </>
   );
 };
